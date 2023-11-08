8a0599a84a6d180db9628e5a4fc045270197bfd3
sabbir7707 <sabbirhosain104@gmail.com>
2023-11-08 19:32:54 +0600

    The delete function is attached and every ID is deleted

--- index.js
+++ index.js
@@ -97,20 +97,14 @@ async function run() {
     }) 
 
 
-
-
- 
-
-
-     
-   /*  app.delete('/app/v1/allassignment/:id', async(req ,res)=>{
+     app.delete('/app/v1/allassignment/:id', async(req ,res)=>{
       const id = req.params.id;
      const query = {_id: new ObjectId(id)}
      const result =await assignmentCollection.deleteOne(query)
       res.send(result);
 
   })
-  */
+  
 
 
     // Send a ping to confirm a successful connection
